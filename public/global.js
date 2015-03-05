aea5a8328a6d91ead1e2555e02b50413dbf1dbc3
Derek George <derek68038@gmail.com>
2015-03-05 11:44:00 -0600

    Finished save function for updated JSON object.

--- public/global.js
+++ public/global.js
@@ -1,8 +1,14 @@
 
 function createObject(id) {
   var js_req = new XMLHttpRequest
-  var change
   js_req.open("get", "http://localhost:4567/students/" + id)
   js_req.send()
   JSON.parse(js_req.response)
-}
+};
+
+function saveObject(object) {
+  var js_req = new XMLHttpRequest
+  js_req.open("get", "http://localhost:4567/students/" + object.id + object.name + object.age + object.github)
+  js_req.send()
+  JSON.parse(js_req.response)
+};
